70d31a36d308a4f696c681c5f33d51b3a088febf
Cesar Colina <tratohecho3@gmail.com>
2024-06-15 13:46:48 -0400

    improve styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "@/app/lib/theme";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ background: "#F0F8FF" }}>
       <body className={inter.className}>
         <AppRouterCacheProvider>
           <ThemeProvider theme={theme}>{children}</ThemeProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,35 @@
 import TaxCalculatorForm from "@/app/components/TaxCalculatorForm/TaxCalculatorForm";
-import { Typography, Grid } from "@mui/material";
+import { Typography, Grid, Paper } from "@mui/material";
 import { taxYearValues } from "@/app/constants/taxYears";
 export default function Home() {
   return (
     <Grid container justifyContent={"center"} marginTop={"50px"}>
       <Grid item xs={10}>
-        <Grid item xs={12}>
-          <Typography
-            variant="h1"
-            sx={{ fontSize: "2rem", fontWeight: "bold", color: "text.primary" }}
-          >
-            Income Tax Calculator
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Typography
-            variant="body2"
-            sx={{ fontSize: "1.1rem", color: "grey.700" }}
-          >
-            Find out how much your salary is after tax
-          </Typography>
-        </Grid>
-        <Grid item xs={12} marginTop="20px">
-          <TaxCalculatorForm taxYearValues={taxYearValues} />
-        </Grid>
+        <Paper elevation={3} sx={{ padding: "20px" }}>
+          <Grid item xs={12}>
+            <Typography
+              variant="h1"
+              sx={{
+                fontSize: "2rem",
+                fontWeight: "bold",
+                color: "text.primary",
+              }}
+            >
+              Income Tax Calculator
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography
+              variant="body2"
+              sx={{ fontSize: "1.1rem", color: "grey.700" }}
+            >
+              Find out how much your salary is after tax
+            </Typography>
+          </Grid>
+          <Grid item xs={12} marginTop="20px">
+            <TaxCalculatorForm taxYearValues={taxYearValues} />
+          </Grid>
+        </Paper>
       </Grid>
     </Grid>
   );
